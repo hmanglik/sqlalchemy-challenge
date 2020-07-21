ac670591744e5f13ecb8ca245da1948e91ef33a8
Himani Manglik <himanimanglik@Himanis-Air.home>
2020-07-21 09:28:42 -0400

    did climate starter hw

---
+++ app.py
@@ -0,0 +1,89 @@
+import numpy as np
+
+import sqlalchemy
+from sqlalchemy.ext.automap import automap_base
+from sqlalchemy.orm import Session
+from sqlalchemy import create_engine, func
+
+from flask import Flask, jsonify
+
+
+#################################################
+# Database Setup
+#################################################
+engine = create_engine("sqlite:///Resources/hawaii.sqlite")
+
+# reflect an existing database into a new model
+Base = automap_base()
+# reflect the tables
+Base.prepare(engine, reflect=True)
+
+# Save reference to the table
+Measurement = Base.classes.measurement
+Station = Base.classes.station
+
+#################################################
+# Flask Setup
+#################################################
+app = Flask(__name__)
+
+
+#################################################
+# Flask Routes
+#################################################
+
+@app.route("/")
+def home():
+    """List all available api routes."""
+    return (
+        f"Available Routes:<br/>"
+        f"/api/v1.0/precipitation<br/>"
+        f"/api/v1.0/stations<br/>"
+        f"/api/v1.0/tobs<br/>"
+        f"/api/v1.0/<start><br/>"
+        f"/api/v1.0/<start>/<end>"
+    )
+
+
+@app.route("/api/v1.0/precipitation")
+def precipitation():
+    # Create our session (link) from Python to the DB
+    session = Session(engine)
+
+    """Return a list of all passenger names"""
+    # Query for dates and precipitation values
+    results = session.query(Measurement.date, Measurement.prcp).\
+        order_by(Measurement.date).all()
+
+    prcp_list = []
+
+    for date, prcp in results:
+        empty_dict = {}
+        empty_dict[date] = prcp
+        prcp_list.append(empty_dict)
+
+    session.close()
+
+    return jsonify(prcp_list)
+
+
+@app.route("/api/v1.0/stations")
+def stations():
+    # Create our session (link) from Python to the DB
+    session = Session(engine)
+
+    """Return a list of passenger data including the name, age, and sex of each passenger"""
+    # Query all passengers
+    empty_dict = {}
+
+    results = session.query(Station.station, Station.name).all()
+
+    for station, name in results:
+        empty_dict[station] = name
+
+    session.close()
+
+    return jsonify (stations) 
+
+if __name__ == '__main__':
+    app.run(debug=True)
