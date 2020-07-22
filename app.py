1529c15ca59b3ca6d71b5a6b59f8305d8edb3e3b
Himani Manglik <himanimanglik@Himanis-Air.home>
2020-07-22 15:47:58 -0400

    added to app.py

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 import numpy as np
+import datetime as dt
 
 import sqlalchemy
 from sqlalchemy.ext.automap import automap_base
@@ -40,8 +41,7 @@ def home():
         f"/api/v1.0/precipitation<br/>"
         f"/api/v1.0/stations<br/>"
         f"/api/v1.0/tobs<br/>"
-        f"/api/v1.0/<start><br/>"
-        f"/api/v1.0/<start>/<end>"
+        f"/api/v1.0/temp/start/end"
     )
 
 
@@ -50,7 +50,6 @@ def precipitation():
     # Create our session (link) from Python to the DB
     session = Session(engine)
 
-    """Return a list of all passenger names"""
     # Query for dates and precipitation values
     results = session.query(Measurement.date, Measurement.prcp).\
         order_by(Measurement.date).all()
@@ -72,18 +71,55 @@ def stations():
     # Create our session (link) from Python to the DB
     session = Session(engine)
 
-    """Return a list of passenger data including the name, age, and sex of each passenger"""
-    # Query all passengers
-    empty_dict = {}
-
     results = session.query(Station.station, Station.name).all()
 
+    station_list = [] 
     for station, name in results:
+        empty_dict = {}
         empty_dict[station] = name
-
+        station_list.append({name: station})
+        #station_list.append(empty_dict)
     session.close()
 
-    return jsonify (stations) 
+    return jsonify(station_list) 
+
+@app.route("/api/v1.0/tobs")
+def tobs():
+    print("here")
+    session = Session(engine)
+    most_recent_date = session.query(Measurement.date).order_by(Measurement.date.desc()).first()
+    most_recent_date = most_recent_date[0]
+    most_recent_date = dt.datetime.strptime(most_recent_date, "%Y-%m-%d")
+    print(most_recent_date)
+    query_date = most_recent_date-dt.timedelta(days = 365)
+    temp_obs = session.query(Measurement.station, func.count(Measurement.tobs)).\
+        group_by(Measurement.station).order_by(func.count(Measurement.tobs).desc()).all()
+    temp_highest = temp_obs[0][0]
+    last_12 = session.query(Measurement.tobs).\
+        filter(Measurement.station == temp_highest).\
+        filter(Measurement.date >= query_date).all()
+    normal_list = list(np.ravel(last_12))
+    session.close()
+    return jsonify(normal_list)
+
+@app.route("/api/v1.0/temp/<start>")
+@app.route("/api/v1.0/temp/<start>/<end>")
+def stats(start = None, end = None):
+    session = Session(engine)
+
+    sel = [func.min(Measurement.tobs), func.avg(Measurement.tobs), func.max(Measurement.tobs)]
+
+    if not end:
+        temps = session.query(*sel).filter(Measurement.date >= start).all()
+        temps_list = list(np.ravel(results))
+        print(temps_list)
+        return jsonify(temps_list)
+        
+    new_temps = session.query(*sel).filter(Measurement.date >= start).filter(Measurement.date <= end).all()
+    print(new_temps)        
+  
+    return jsonify(new_temps)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True)  
+     
